42842bd3de59b2cc8ac5061a327111f34228b0cf
Hayden Betts <haydenbetts@gmail.com>
2017-09-25 13:58:52 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,8 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  # notice no '/' before posts
+  resources :posts, only: :show
+
 end
